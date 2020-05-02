fc7926bf884e77b56279c086d49510b58763e914
SACHIN-WEBDEV <sachinkumar.webdev@gmail.com>
2020-05-03 02:38:23 +0530

    Deploy

---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyArbjjgjQj5ABT9atT0ZH0Dwha0PI-fhSw",
+  authDomain: "database-3cd4b.firebaseapp.com",
+  databaseURL: "https://database-3cd4b.firebaseio.com",
+  projectId: "database-3cd4b",
+  storageBucket: "database-3cd4b.appspot.com",
+  messagingSenderId: "7647286481",
+  appId: "1:7647286481:web:a8231584bea42f548ca1f0",
+  measurementId: "G-SPPYE21BFH",
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
